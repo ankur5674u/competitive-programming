8389215cc1e6d2559a91d50636d937ed1c0b2bc4
Ankur Prakash Singh <ankur5674u@gmail.com>
2019-10-11 08:57:18 +0530

    Jadoo vs Koba

---
+++ hackerearth/python_practice/jadoo_vs_koba.py
@@ -0,0 +1,16 @@
+"""
+https://www.hackerearth.com/practice/python/getting-started/input-and-output/practice-problems/golf/jadoo-vs-koba/
+"""
+
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+for i in range(ord('F'),ord('Q')):
+    print(i)
