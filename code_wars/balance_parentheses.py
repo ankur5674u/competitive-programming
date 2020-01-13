05ac75841ceb765ca8f193ba24ccbfd4784cf1f0
Ankur Singh <ankur.singh@incentius.com>
2020-01-13 10:48:31 +0530

    Added balance parenthesis

--- code_wars/balance_parentheses.py
+++ code_wars/balance_parentheses.py
@@ -2,6 +2,23 @@ __author__ = "Ankur Prakash Singh"
 # Date format "%m-%d-%Y"
 __date__ = '01-13-2020'
 
+"""
+https://www.codewars.com/kata/52774a314c2333f0a7000688/train/python
+
+Write a function called that takes a string of parentheses, and
+determines if the order of the parentheses is valid. 
+The function should return true if the string is valid, 
+and false if it's invalid.
+
+Along with opening (() and closing ()) parenthesis, 
+input may contain any valid ASCII characters. 
+Furthermore, the input string may be empty and/or not contain 
+any parentheses at all. Do not treat other forms of brackets 
+as parentheses (e.g. [], {}, <>).
+
+
+"""
+
 
 def is_valid_parentheses(input_p):
     """
@@ -17,10 +34,12 @@ def is_valid_parentheses(input_p):
     # Declaring empty stack
     stack = []
     input_paren_l = ['(', '{', '[']
+    closing_paren_l = [')', '}', ']']
+
     for paren in input_p:
         if paren in input_paren_l:
             stack.append(paren)
-        else:
+        elif paren in closing_paren_l:
             if not stack:
                 return False
             else:
@@ -29,6 +48,8 @@ def is_valid_parentheses(input_p):
                         paren == ']' and top == '[' or \
                         paren == '}' and top == '{':
                     stack.pop()
+        else:
+            continue
     return True if not stack else False
 
 
